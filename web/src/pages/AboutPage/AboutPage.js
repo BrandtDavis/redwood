ab08f0a1411e8e83474d8b155f251ac428a18e5a
Brandt Davis <brandtdavis@Brandts-MacBook-Air.local>
2022-06-25 13:43:05 -0600

    Add 2 pages and basic layout structure

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>This site is real neato</p>
+    </>
+  )
+}
+
+export default AboutPage

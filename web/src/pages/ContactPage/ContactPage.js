773869b733a6981d03e80ce96bb500e5471f2272
Brandt Davis <brandtdavis@Brandts-MacBook-Air.local>
2022-07-11 06:27:12 -0600

    Add error handling to contact form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,5 +1,12 @@
 import { MetaTags } from '@redwoodjs/web'
-import { Form, Label, TextAreaField, TextField, Submit } from '@redwoodjs/forms'
+import {
+  FieldError,
+  Form,
+  Label,
+  TextAreaField,
+  TextField,
+  Submit
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
   const onSubmit = (data) => {
@@ -9,15 +16,42 @@ const ContactPage = () => {
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
-      <Form onSubmit={onSubmit}>
-        <Label htmlFor="name">Name</Label>
-        <TextField name="name" />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required:true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
 
-        <Label htmlFor="email">Email</Label>
-        <TextField name="email" />
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
+            }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
 
-        <Label htmlFor="message">Message</Label>
-        <TextAreaField name="message" />
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required:true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
         <Submit>Save</Submit>
       </Form>
     </>
